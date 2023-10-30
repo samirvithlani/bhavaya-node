bf097253ecc442ce75ad315755cf29f99864fa0a
samirvithlani <samir.vithlani83955@gmail.com>
2023-10-30 16:30:53 +0530

    app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
 
@@ -8,62 +9,21 @@ const PORT = 3000;
 //GET,POST,PUT,DELETE
 
 //service --> req,res
-app.get("/user", (req, res) => {
-  var users = [
-    {
-      id: 1,
-      name: "bhavya",
-    },
-    {
-      id: 2,
-      name: "raj",
-    },
-  ];
-  console.log("user api called....");
-  //res.send("api called...")
-  //res.status(402).send("api called..")
-  res.status(201).json({
-    message: "user fetch successfully..",
-    data: users,
-  });
-});
 
-app.get("/user/:uid",(req,res)=>{
 
-    var id = req.params.uid
-    console.log("id = ",id)
-    res.status(200).json({
-        message:"id received.",
-        id:id
-    })
+
+//db connection
+const db  = mongoose.connect("mongodb://127.0.0.1:27017/bhavya_node",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+db.then(() => {
+  console.log("connected to db");
+}).catch((err) => {
+  console.log("error", err);
 })
 
-app.get("/filuser/:id",(req,res)=>{
-var id = req.params.id;
-    var users =[
-        {
-            id:1,
-            name:"bhavaya"
-        },{
-            id:2,
-            name:"raj"
-        },
-        {
-            id:3,
-            name:"amit"
-        }
-    ]
 
-    users = users.filter((user)=>{
-        return user.id!=id;
-    })
-    console.log(users)
-    res.status(201).json({
-        message:"data filtered..",
-        data:users
-    })
-
-})
 app.listen(PORT, () => {
   console.log("server started on port", 3000);
 });
--- backuoapp.js
+++ backuoapp.js
@@ -14,3 +14,60 @@ console.log(ans)
 console.log(name)
 console.log(age)
 console.log("end of node 1")
+app.get("/user", (req, res) => {
+    var users = [
+      {
+        id: 1,
+        name: "bhavya",
+      },
+      {
+        id: 2,
+        name: "raj",
+      },
+    ];
+    console.log("user api called....");
+    //res.send("api called...")
+    //res.status(402).send("api called..")
+    res.status(201).json({
+      message: "user fetch successfully..",
+      data: users,
+    });
+  });
+  
+  app.get("/user/:uid",(req,res)=>{
+  
+      var id = req.params.uid
+      console.log("id = ",id)
+      res.status(200).json({
+          message:"id received.",
+          id:id
+      })
+  })
+  
+  app.get("/filuser/:id",(req,res)=>{
+  var id = req.params.id;
+      var users =[
+          {
+              id:1,
+              name:"bhavaya"
+          },{
+              id:2,
+              name:"raj"
+          },
+          {
+              id:3,
+              name:"amit"
+          }
+      ]
+  
+      users = users.filter((user)=>{
+          return user.id!=id;
+      })
+      console.log(users)
+      res.status(201).json({
+          message:"data filtered..",
+          data:users
+      })
+  
+  })
+  
